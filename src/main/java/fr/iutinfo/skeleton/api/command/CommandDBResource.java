4b3ca4a071b6f6b4ad6d6dce1a400107ab5ae704
kermekx <kermekx@gmail.com>
2016-03-30 08:25:35 +0200

    access to command

--- src/main/java/fr/iutinfo/skeleton/api/command/CommandDBResource.java
+++ src/main/java/fr/iutinfo/skeleton/api/command/CommandDBResource.java
@@ -34,10 +34,11 @@ public class CommandDBResource {
 
 	public CommandDBResource() {
 		try {
-			//dao.dropCommandTable();
+			// dao.dropCommandTable();
 			dao.createCommandTable();
-			//dao.insert(new Command(0, 3, "rue du test unitaire", "rue du bug", "02/10/16 17:30", "03/10/16 17:30", "50.0",
-			//		"LINGE_QUOTIDIEN_REPASSAGE_PLIAGE:1;LINGE_QUOTIDIEN_LAVAGE_SECHAGE:2"));
+			// dao.insert(new Command(0, 3, "rue du test unitaire", "rue du
+			// bug", "02/10/16 17:30", "03/10/16 17:30", "50.0",
+			// "LINGE_QUOTIDIEN_REPASSAGE_PLIAGE:1;LINGE_QUOTIDIEN_LAVAGE_SECHAGE:2"));
 		} catch (Exception e) {
 			System.out.println("Table déjà là !");
 		}
@@ -53,12 +54,12 @@ public class CommandDBResource {
 					.entity("Ressource requires login.").build());
 		}
 
-	/*	if (command.getUserid() != currentUser.getId()) {
+		if (command.getUserid() != currentUser.getId()) {
 			throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED)
 					.header(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Take & Wash\"")
 					.entity("Cannot access to this ressource.").build());
 		}
-*/
+
 		command.setPaid(0);
 
 		// calculate command price
@@ -78,12 +79,12 @@ public class CommandDBResource {
 				}
 			}
 		}
-		
-		command.setPrice(String.format("%.2f",total).replaceAll(",", "."));
 
-		System.err.println("command.getDateLivraison(): "+command.getDateLivraison());
+		command.setPrice(String.format("%.2f", total).replaceAll(",", "."));
+
+		System.err.println("command.getDateLivraison(): " + command.getDateLivraison());
 		System.out.println(command.toString());
-		
+
 		int id = dao.insert(command);
 		command.setId(id);
 		return command;
@@ -105,14 +106,13 @@ public class CommandDBResource {
 		if (command == null) {
 			throw new WebApplicationException(404);
 		}
-/*
-		if (command.getUserid() != currentUser.getId()) {
-			throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED)
-					.header(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Take & Wash\"")
-					.entity("Cannot access to this ressource.").build());
-		}
-*/
-		return command;
+
+		if (command.getUserid() == currentUser.getId() || User.isAdmin(currentUser))
+			return command;
+
+		throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED)
+				.header(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Take & Wash\"")
+				.entity("Cannot access to this ressource.").build());
 	}
 
 	@PUT
@@ -131,13 +131,13 @@ public class CommandDBResource {
 		if (oldUser == null) {
 			throw new WebApplicationException(404);
 		}
-/*
+
 		if (command.getUserid() != currentUser.getId()) {
 			throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED)
 					.header(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Take & Wash\"")
 					.entity("Cannot access to this ressource.").build());
 		}
-*/
+
 		dao.update(command);
 		return command;
 	}
@@ -152,7 +152,7 @@ public class CommandDBResource {
 					.entity("Ressource requires login.").build());
 		}
 
-		return dao.findAllByUserId(currentUser.getId());
+		return User.isAdmin(currentUser) ? dao.all() : dao.findAllByUserId(currentUser.getId());
 	}
 
 }
